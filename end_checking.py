c7e0bc0e5ca348abcc68de52888d936803b66bc6
Pranav Jagada <pranavj918@gmail.com>
2023-07-11 14:21:05 -0500

    remove unnecessary code

--- end_checking.py
+++ end_checking.py
@@ -1,28 +1,5 @@
-import tail
-from watcher import Watcher
-
 INSTANCES_DIRECTORY = 'C:/MultiMC/instances'
 INSTANCES = ["/1.17.1_1", "/1.17.1_2", "/1.17.1_3"]
 LOG_FILE = "/.minecraft/logs/latest.log"
 
 LOG_FILES = [INSTANCES_DIRECTORY + instance + LOG_FILE for instance in INSTANCES]
-last_end_lines = {}
-for log_file in LOG_FILES:
-    last_end_lines[log_file] = ""
-
-def send_pacepal():
-	print("peej has entered the end!")
-
-def compare_and_update_line(file, line):
-    if last_end_lines[file] != line:
-        last_end_lines[file] = line
-        send_pacepal()
-
-def check_log_file(file):
-	print("log file updated")
-	lines = tail(file, 25)
-	for line in lines:
-		if "has made the advancement [The End?]" in line:
-			compare_and_update_line(file, line)
-
-# watchers = [Watcher(log_file, check_log_file, file=log_file) for log_file in log_files]
--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import asyncio
 from end_checking import LOG_FILES
-from tail import tail
 
 # IMPORT DISCORD.PY. ALLOWS ACCESS TO DISCORD'S API.
 import discord
 from discord.ext import tasks
 
 # IMPORT THE OS MODULE.
-import os, sys, time
+import os
 
 # IMPORT LOAD_DOTENV FUNCTION FROM DOTENV MODULE.
 from dotenv import load_dotenv
@@ -29,31 +27,17 @@ stamps = [0 for i in range(len(LOG_FILES))]
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        # CREATES A COUNTER TO KEEP TRACK OF HOW MANY GUILDS / SERVERS THE BOT IS CONNECTED TO.
         guild_count = 0
-
-        # LOOPS THROUGH ALL THE GUILD / SERVERS THAT THE BOT IS ASSOCIATED WITH.
         for guild in bot.guilds:
-            # PRINT THE SERVER'S ID AND NAME.
             print(f"- {guild.id} (name: {guild.name})")
-
-            # INCREMENTS THE GUILD COUNTER.
             guild_count = guild_count + 1
-
-        # PRINTS HOW MANY GUILDS / SERVERS THE BOT IS IN.
         print("SampleDiscordBot is in " + str(guild_count) + " guilds.")
-
-    async def on_message(self, message):
-        # CHECKS IF THE MESSAGE THAT WAS SENT IS EQUAL TO "HELLO".
-        if message.content == "hello":
-            # SENDS BACK A MESSAGE TO THE CHANNEL.
-            await message.channel.send("hey dirtbag")
     
     async def setup_hook(self) -> None:
         # start the task to run in the background
         self.my_background_task.start()
 
-    @tasks.loop(seconds=2)  # task runs every 60 seconds
+    @tasks.loop(seconds=2)  # task runs every 2 seconds
     async def my_background_task(self):
         channel = self.get_channel(986431075217707021)  # channel ID goes here
         for i in range(len(LOG_FILES)):
@@ -80,9 +64,5 @@ class MyClient(discord.Client):
 
 # GETS THE CLIENT OBJECT FROM DISCORD.PY. CLIENT IS SYNONYMOUS WITH BOT.
 bot = MyClient(intents=intents)
-
-# EXECUTES THE BOT WITH THE SPECIFIED TOKEN. TOKEN HAS BEEN REMOVED AND USED JUST AS AN EXAMPLE.
-def run_bot():
-    bot.run(DISCORD_TOKEN)
-
-run_bot()
+# EXECUTES THE BOT WITH THE SPECIFIED TOKEN.
+bot.run(DISCORD_TOKEN)
