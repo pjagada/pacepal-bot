dc7db2fdccaf2ff3159d2016f5f1c8859b39671f
Pranav Jagada <pranavj918@gmail.com>
2023-07-11 14:32:09 -0500

    change channel to pacepal channel

--- custom_things.py
+++ custom_things.py
@@ -2,7 +2,7 @@ INSTANCES_DIRECTORY = 'C:/MultiMC/instances'
 INSTANCES = ["/1.17.1_1", "/1.17.1_2", "/1.17.1_3"]
 ADVANCEMENT = "The End?"
 MESSAGE = "gravel seed end enter https://www.twitch.tv/peej918"
-CHANNEL_ID = 986431075217707021
+CHANNEL_ID = 946866237487587368
 
 # do not edit
 LOG_FILE = "/.minecraft/logs/latest.log"
